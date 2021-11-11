189dab40028d1345038ef1bdc01f4c535133f5e1
arpita1329 <arpitadubey1329@gmail.com>
2021-11-11 09:23:34 +0530

    Portfolio section completed

---
+++ src/data.js
@@ -0,0 +1,139 @@
+export const featuredPortfolio = [
+    {
+        id: 1,
+        title: "Zomato - Home Landing Page",
+        img: "assets/zomato.png",
+    },
+    {
+        id: 2,
+        title: "Book management API",
+        img: "assets/zomato.png",
+    },
+    {
+        id: 3,
+        title: "Tasky - To Do App",
+        img: "assets/zomato.png",
+    },
+    {
+        id: 4,
+        title: "Book my Show - Clone",
+        img: "assets/zomato.png",
+    },
+    {
+        id: 5,
+        title: "Zomato clone",
+        img: "assets/zomato.png",
+    },
+]
+
+export const webPortfolio = [
+    {
+        id: 1,
+        title: "Zomato - Home Landing Page",
+        img: "assets/zomato.png",
+    },
+    {
+        id: 2,
+        title: "Book management API",
+        img: "assets/zomato.png",
+    },
+    {
+        id: 3,
+        title: "Tasky - To Do App",
+        img: "assets/zomato.png",
+    },
+    {
+        id: 4,
+        title: "Book my Show - Clone",
+        img: "assets/zomato.png",
+    },
+    {
+        id: 5,
+        title: "Zomato clone",
+        img: "assets/zomato.png",
+    },
+]
+
+export const mobilePortfolio = [
+    {
+        id: 1,
+        title: "Zomato - Home Landing Page",
+        img: "assets/zomato.png",
+    },
+    {
+        id: 2,
+        title: "Book management API",
+        img: "assets/zomato.png",
+    },
+    {
+        id: 3,
+        title: "Tasky - To Do App",
+        img: "assets/zomato.png",
+    },
+    {
+        id: 4,
+        title: "Book my Show - Clone",
+        img: "assets/zomato.png",
+    },
+    {
+        id: 5,
+        title: "Zomato clone",
+        img: "assets/zomato.png",
+    },
+]
+
+export const designPortfolio = [
+    {
+        id: 1,
+        title: "Zomato - Home Landing Page",
+        img: "assets/zomato.png",
+    },
+    {
+        id: 2,
+        title: "Book management API",
+        img: "assets/zomato.png",
+    },
+    {
+        id: 3,
+        title: "Tasky - To Do App",
+        img: "assets/zomato.png",
+    },
+    {
+        id: 4,
+        title: "Book my Show - Clone",
+        img: "assets/zomato.png",
+    },
+    {
+        id: 5,
+        title: "Zomato clone",
+        img: "assets/zomato.png",
+    },
+]
+
+export const contentPortfolio = [
+    {
+        id: 1,
+        title: "Zomato - Home Landing Page",
+        img: "assets/zomato.png",
+    },
+    {
+        id: 2,
+        title: "Book management API",
+        img: "assets/zomato.png",
+    },
+    {
+        id: 3,
+        title: "Tasky - To Do App",
+        img: "assets/zomato.png",
+    },
+    {
+        id: 4,
+        title: "Book my Show - Clone",
+        img: "assets/zomato.png",
+    },
+    {
+        id: 5,
+        title: "Zomato clone",
+        img: "assets/zomato.png",
+    },
+]
