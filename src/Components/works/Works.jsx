a48d2e9a097399831194e7dd9a4548626ebe4224
arpita1329 <arpitadubey1329@gmail.com>
2021-11-12 16:58:40 +0530

    Work section completed

--- src/Components/works/Works.jsx
+++ src/Components/works/Works.jsx
@@ -1,9 +1,62 @@
 import "./works.scss"
+import { useState } from "react";
+import { ChevronLeft, ChevronRight} from "@material-ui/icons";
 
 export default function Works() {
+    const [ currentSlide, setCurrentSlide ] = useState(0);
+    const data = [
+        {
+            id: "1",
+            icon: "assets/mobile.png",
+            title: "Web Design",
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, debitis quasi temporibus fuga repellendus quos recusandae, odio laboriosam quam commodi fugit tempore porro ipsa voluptatum est provident et reiciendis cumque!",
+            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHjqV-cmWdF_CHkspXrTo5v0_tHydyNTo9_w",
+        },
+        {
+            id: "2",
+            icon: "assets/globe.jpg",
+            title: "Mobile Application",
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, debitis quasi temporibus fuga repellendus quos recusandae, odio laboriosam quam commodi fugit tempore porro ipsa voluptatum est provident et reiciendis cumque!",
+            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1QRtDv1PfEpZVmQlki9VH2-ECScaNHXUPdQ&usqp=CAU",
+        },
+        {
+            id: "3",
+            icon: "assets/writing.png",
+            title: "Branding",
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, debitis quasi temporibus fuga repellendus quos recusandae, odio laboriosam quam commodi fugit tempore porro ipsa voluptatum est provident et reiciendis cumque!",
+            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIlXYAYUF_D_otOsGB1qvOdCUyqMI2Mf_23Q&usqp=CAU",
+        },
+    ];
+
+    const handledClick = (way) => {
+        way === "left" ? setCurrentSlide(currentSlide > 0 ? currentSlide-1 : 2) : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide+1 : 0);
+    }
+
     return (
         <div className="works" id="works">
-            
+            <div className="slider" style={{ transform: `translateX(-${currentSlide * 100}vw)` }}>
+                { data.map(d=> (
+                  <div className="container">
+                    <div className="item">
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    <img src={d.icon} alt="" />
+                                </div>
+                                <h2>{d.title}</h2>
+                                <p>{d.desc}</p>
+                                <span>Projects</span>
+                            </div>
+                        </div>
+                        <div className="right">
+                            <img src={d.img} alt="project" />
+                        </div>
+                    </div>
+                  </div>
+                ))}
+            </div>
+            <ChevronLeft className="arrow left" onClick={()=>handledClick("left")} />
+            <ChevronRight className="arrow right"  onClick={()=>handledClick("right")} />
         </div>
     )
 }
